032050f7b2433299e4707004b1941df00b10d761
Ryotaro Sakai <sakai@cloudbuilders.jp>
2025-12-08 13:06:18 +0000

    infra: Lambda (DockerImageFunction) と CloudFront (OAC) を追加
    - Dockerfile から Lambda 関数を作成
    - Function URL (AWS_IAM) を有効化
    - CloudFront Distribution を作成し、Origin Access Control (OAC) を設定

--- packages/infra/lib/tan-stack-lambda-demo-stack.ts
+++ packages/infra/lib/tan-stack-lambda-demo-stack.ts
@@ -1,8 +1,37 @@
 import * as cdk from "aws-cdk-lib";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 import type { Construct } from "constructs";
+import * as path from "path";
 
 export class TanStackLambdaDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const handler = new lambda.DockerImageFunction(this, "Handler", {
+      code: lambda.DockerImageCode.fromImageAsset(
+        path.join(__dirname, "../../../"),
+      ),
+      memorySize: 512,
+      timeout: cdk.Duration.seconds(30),
+      architecture: lambda.Architecture.ARM_64,
+    });
+
+    const fnUrl = handler.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.AWS_IAM,
+    });
+
+    new cloudfront.Distribution(this, "Distribution", {
+      defaultBehavior: {
+        origin: origins.FunctionUrlOrigin.withOriginAccessControl(fnUrl),
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+    });
+
+    new cdk.CfnOutput(this, "FunctionUrl", {
+      value: fnUrl.url,
+    });
   }
 }
